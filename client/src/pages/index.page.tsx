2b7fa33fae203be3172b35434defe6486ca213d3
Hedrs <hedrs@yandex.ru>
2025-06-17 11:31:53 +0300

    Test 1.7

--- client/src/pages/index.page.tsx
+++ client/src/pages/index.page.tsx
@@ -4,6 +4,11 @@ function Page() {
       <h1>Notis call</h1>
       <div>AHAHHAHAHAHHAHAAHAHAHHAHAHAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA</div>
       <input type="text" name="puk" id="puk" placeholder="puk" />
+      <p>aEDedaseddefdf</p>
+      <p>
+        sjhcvbakdjfbvlqfbvlhfbvpbfgvufbvhadbcvhbqelfhvbch bytbpqirfpqeyfbvltviq erviqer vq ervyqer vpiyq er vqervqeri
+      </p>
+      <p>skchbalsdfvbqeyirfb qeyif r r </p>
     </div>
   );
 }
